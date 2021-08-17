32912b1a06d6da35c5868b5ec7c6d2b81f813b5e
Vitaliy <vialiy.inc.if.uk@gmail.com>
2021-08-18 00:44:05 +0300

    add homework 2

---
+++ C_language_part_02/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+int *input_array(size_t *size);
+int biggest(int *arr, int count);
+void print_arr(int *arr, size_t *size);
